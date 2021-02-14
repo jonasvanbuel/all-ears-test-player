9b47f57d35762a95556ea286e12491dfbcc6ee73
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-02-14 10:07:23 +0000

    styling scss implementation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'react-app-polyfill/ie9';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
