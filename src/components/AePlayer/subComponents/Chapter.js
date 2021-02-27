f855652ee1fab4bab09bda94a978dd65cbe057df
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-02-27 12:54:58 +0000

    html5 audio element with custom pButton

--- src/components/AePlayer/subComponents/Chapter.js
+++ src/components/AePlayer/subComponents/Chapter.js
@@ -1,13 +1,46 @@
-// import React from 'react';
+import React, { useState, useEffect } from 'react';
+import {Howl, Howler} from 'howler';
+
 import PpButton from './PpButton';
 
+
 const Chapter = ({chapter}) => {
-  const { count, title } = chapter;
+  const { count, title, audioSrc } = chapter;
+  const [ playing, setPlaying ] = useState(false);
+
+  let audio = null;
+  let pButton = null;
+
+  useEffect(() => {
+    audio = document.getElementById(`ch-${count}`).getElementsByTagName('audio')[0];
+    pButton = document.getElementById(`ch-${count}`).getElementsByClassName('pButton')[0];
+  }, []);
+
+
+
+  const playAudio = () => {
+    if (audio.paused) {
+      audio.play();
+      pButton.className = "";
+      pButton.className = "pButton pause";
+    } else {
+      audio.pause();
+      pButton.className = "";
+      pButton.className = "pButton play";
+    }
+  }
 
   return (
-    <div className="chapter">
+    <div id={`ch-${count}`} className="chapter">
       <p className="chapter-title">{`Chapter ${count}: ${title}`}</p>
-      <PpButton playing={chapter.playing} />
+
+      <audio>
+        <source src={audioSrc[0]} type="audio/webm" />
+        <source src={audioSrc[1]} type="audio/mpeg" />
+      </audio>
+
+      <button className="pButton play" onClick={playAudio}></button>
+
     </div>
   );
 }
