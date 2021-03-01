6f5a49087284f87a31007b00f257cd9c94dd38ac
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-03-01 18:40:32 +0000

    extended datafeed with second chapter for testing

--- src/context/PlayerContext.js
+++ src/context/PlayerContext.js
@@ -18,6 +18,17 @@ const dataFeed = {
         'https://res.cloudinary.com/deo4sjfc8/video/upload/v1613466275/2021-all-ears-test-player/audio/05-Piano_tps3ou.webm',
         'https://res.cloudinary.com/deo4sjfc8/video/upload/v1613495616/2021-all-ears-test-player/audio/05-Piano_nqmair.mp3'
       ]
+    },
+    {
+      id: 2,
+      count: 2,
+      title: 'Miles Davis - Flamenco Sketches',
+      playing: false,
+      img: '',
+      audioSrc: [
+        'https://res.cloudinary.com/deo4sjfc8/video/upload/v1614623913/2021-all-ears-test-player/audio/Miles_uitqur.webm',
+        'https://res.cloudinary.com/deo4sjfc8/video/upload/v1614623816/2021-all-ears-test-player/audio/Miles_bgr9sx.mp3'
+      ]
     }
   ]
 }
