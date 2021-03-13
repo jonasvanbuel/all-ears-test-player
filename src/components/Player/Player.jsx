96ef219642f3164128753c1b3b7616a3b3f7b5fe
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-03-13 13:41:21 +0000

    initial setup

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,17 +1,43 @@
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
 import './Player.scss';
 
 import usePlayer from './hooks/usePlayer';
 import Chapter from './subComponents/Chapter';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination]);
 
 const Player = () => {
   const { chapters } = usePlayer();
 
   return (
     <div id="player">
-      {chapters ? chapters.map((chapter) => (
-        <Chapter key={chapter.number} chapter={chapter}/>
-      )) :
-      null}
+      <Swiper
+        slidesPerView={1}
+        navigation
+        pagination
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+        passiveListeners={false}
+        preventClicks={false}
+        preventClicksPropagation={false}
+      >
+        {
+          chapters && chapters.length > 1 ? chapters.map((chapter) => (
+            <SwiperSlide
+              style={{width: "100%"}}
+            >
+              <Chapter key={chapter.number} chapter={chapter} />
+            </SwiperSlide>
+          )) :
+          <Chapter key={chapters[0].number} chapter={chapters[0]}/>
+        }
+      </Swiper>
     </div>
   );
 }
--- src/components/Player/subComponents/ProgressCircle.js
+++ src/components/Player/subComponents/ProgressCircle.js
@@ -12,39 +12,37 @@ const ProgressCircle = (props) => {
   } = useProgressCircle(props)
 
   return (
-    <div>
-      <svg
-        id={`ch-${number}-progress-circle`}
-        className="progress-circle"
-        viewBox="0 0 100 100"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <g className="circle-container">
-          <circle
-            className="total-time"
-            cx="50" cy="50" r="45"
-            onClick={handleClick}
-            onMouseMove={handleMouseMove}
-            onTouchMove={handleTouchMove}
-          >
-          </circle>
-          <path
-            className="time-elapsed hide"
-            strokeDasharray={`${0} ${circumference}`}
-            onClick={handleClick}
-            onMouseMove={handleMouseMove}
-            onTouchMove={handleTouchMove}
-            d="
-              M 50, 50
-              m -45, 0
-              a 45,45 0 1,0 90,0
-              a 45,45 0 1,0 -90,0
-            "
-          >
-          </path>
-        </g>
-      </svg>
-    </div>
+    <svg
+      id={`ch-${number}-progress-circle`}
+      className="progress-circle"
+      viewBox="0 0 100 100"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g className="circle-container">
+        <circle
+          className="total-time"
+          cx="50" cy="50" r="45"
+          onClick={handleClick}
+          onMouseMove={handleMouseMove}
+          onTouchMove={handleTouchMove}
+        >
+        </circle>
+        <path
+          className="time-elapsed hide"
+          strokeDasharray={`${0} ${circumference}`}
+          onClick={handleClick}
+          onMouseMove={handleMouseMove}
+          onTouchMove={handleTouchMove}
+          d="
+            M 50, 50
+            m -45, 0
+            a 45,45 0 1,0 90,0
+            a 45,45 0 1,0 -90,0
+          "
+        >
+        </path>
+      </g>
+    </svg>
   )
 }
 
