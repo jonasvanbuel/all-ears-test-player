97e635059c9d651ac7cfe506ce2f6285bacdbe1c
Jonas Vanbuel <jonas.vanbuel@gmail.com>
2021-03-29 16:17:06 +0100

    overscroll behaviour + position for safari

--- src/Player/subComponents/Socials.js
+++ src/Player/subComponents/Socials.js
@@ -3,8 +3,8 @@ import '../styles/socials.scss';
 const Socials = () => {
   return (
     <div className="socials">
-      <i class="fas fa-bookmark"></i>
-      <i class="fas fa-share-alt"></i>
+      <i className="fas fa-bookmark"></i>
+      <i className="fas fa-share-alt"></i>
     </div>
   )
 }
